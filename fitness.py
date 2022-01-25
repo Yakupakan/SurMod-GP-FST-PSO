6389ab7c964c93ab48fe9cfd41e70d834f61be11
GloriaPietropolli <gloria.pietropolli@gmail.com>
2022-01-25 17:31:25 +0100

    first trial :)

---
+++ fitness.py
@@ -0,0 +1,12 @@
+import gplearn
+import fstpso
+
+
+def _fstpso_loss(y, y_pred, w):
+    """Calculate the mean absolute percentage error."""
+    diffs = np.abs(np.divide((np.maximum(0.001, y) - np.maximum(0.001, y_pred)),
+                             np.maximum(0.001, y)))
+    return 100. * np.average(diffs, weights=w)
+
+
+fstpso_loss = make_fitness(_fstpso_loss, greater_is_better=False)
