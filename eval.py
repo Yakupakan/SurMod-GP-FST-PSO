5459f41aaca89c2984ed9011d6b9beba539004ab
GloriaPietropolli <gloria.pietropolli@gmail.com>
2022-01-27 17:07:33 +0100

    make_function implementation

--- eval.py
+++ eval.py
@@ -38,6 +38,51 @@ def eval(stack, program):
     return stack
 
 
+def make_function(program):
+    """
+    Function that takes a program as input and returns a function that perform the described program
+    :param program: program considered
+    :return: function that emulates the program
+    """
+
+    def function_from_prgr(x, program=program):
+        stack = x
+        while program:
+            op = program[0]
+            program = program[1:]
+            if op == opcodes.PLUS:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                stack.append(op1 + op2)
+            elif op == opcodes.MINUS:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                stack.append(op1 - op2)
+            elif op == opcodes.TIMES:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                stack.append(op1 * op2)
+            elif op == opcodes.DIVIDE:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                stack.append(op1 / op2)
+            elif op == opcodes.MOD:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                stack.append(op1 % op2)
+            elif op == opcodes.DUP:
+                tmp = stack.pop()
+                stack.append(tmp)
+                stack.append(tmp)
+            elif op == opcodes.NOP:
+                pass
+            else:
+                stack.append(op)
+        return stack
+
+    return lambda x: function_from_prgr(x)
+
+
 """"
         elif op == opcodes.SWAP:
             tmp1 = stack.pop()
