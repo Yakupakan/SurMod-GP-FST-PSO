e82fe9588b34de04760f701d7abf1933a843e81e
GloriaPietropolli <gloria.pietropolli@gmail.com>
2022-02-02 11:29:27 +0100

    plot.py implementation

---
+++ plot.py
@@ -0,0 +1,21 @@
+import os
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.set(context='notebook', style='whitegrid', palette='deep', font='sans-serif', font_scale=1)
+
+
+def fitness_plot(dir):
+    print(os.getcwd())
+    with open(dir + "loss.txt") as f:
+        lines = f.readlines()
+    fitness = [float(line[:-2]) for line in lines]
+
+    plt.plot(fitness)
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness")
+    plt.show()
+    plt.savefig(dir + "fitness.png")
+    plt.close()
